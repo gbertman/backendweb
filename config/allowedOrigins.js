c3f09685d95693a51a7da09bb33a9126015a577b
gbertman <gbertman@bertmanlaw.com>
2023-04-29 10:11:10 -0500

    added cors address

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://https://dazzling-snickerdoodle-777101.netlify.app',
+    'https://dazzling-snickerdoodle-777101.netlify.app',
     'http://127.0.0.1:3500',
     'http://localhost:3500'
 ];
